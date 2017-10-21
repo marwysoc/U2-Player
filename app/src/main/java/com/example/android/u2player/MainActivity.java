029a7ad77ceefa8e6338d313e265e8df7a6cbe98
marwysoc <marta.wysocka01@gmail.com>
2017-10-21 17:31:27 +0200

    Move play/pause into methods. Add progress update (textview work, seekBar not yet)

--- app/src/main/java/com/example/android/u2player/MainActivity.java
+++ app/src/main/java/com/example/android/u2player/MainActivity.java
@@ -1,14 +1,13 @@
 package com.example.android.u2player;
 
 import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.util.concurrent.TimeUnit;
 
@@ -21,7 +20,9 @@ public class MainActivity extends AppCompatActivity {
     private MediaPlayer mMediaPlayer;
 
     private double startTime = 0;
-    private double finalTime = 0;
+    private double durationTime = 0;
+
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,58 +45,77 @@ public class MainActivity extends AppCompatActivity {
         time1TextView = (TextView) findViewById(R.id.time1);
         time2TextView = (TextView) findViewById(R.id.time2);
 
+        mHandler = new Handler();
         mMediaPlayer = MediaPlayer.create(this, R.raw.track01);
 
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // start playing sound
-                mMediaPlayer.start();
-
-                // get the duration time of the sound
-                finalTime = mMediaPlayer.getDuration();
-
-                // get the current time of the sound
-                startTime = mMediaPlayer.getCurrentPosition();
-
-                // set the current time to the textview
-                time1TextView.setText(String.format("%d min %d s",
-                        TimeUnit.MILLISECONDS.toMinutes((long) startTime),
-                        TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
-                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long)
-                                        startTime))));
-
-                // set the duration time to the textview
-                time2TextView.setText(String.format("%d min %d s",
-                        TimeUnit.MILLISECONDS.toMinutes((long) finalTime),
-                        TimeUnit.MILLISECONDS.toSeconds((long) finalTime) -
-                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long)
-                                        finalTime))));
-
-                // set progress to the seekbar
-                seekBar.setProgress((int)startTime);
-
-                // enable the pause button when playing sound
-                pauseButton.setEnabled(true);
-
-                // disable the play button - sound is already playing
-                playButton.setEnabled(false);
+                playMusic();
             }
         });
 
         pauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // pause the sound
-                mMediaPlayer.pause();
-
-                // disable puase button - sound is already paused
-                pauseButton.setEnabled(false);
-
-                // enable play button
-                playButton.setEnabled(true);
+                pauseMusic();
             }
         });
+    }
 
+    private void pauseMusic() {
+        // pause the sound
+        mMediaPlayer.pause();
+        // disable puase button - sound is already paused
+        pauseButton.setEnabled(false);
+        // enable play button
+        playButton.setEnabled(true);
+    }
+
+    private void playMusic() {
+        // start playing sound
+        mMediaPlayer.start();
+        // get the duration time of the sound
+        durationTime = mMediaPlayer.getDuration();
+
+        // set the duration time to the textview
+        time2TextView.setText(String.format("%d min %d s",
+                TimeUnit.MILLISECONDS.toMinutes((long) durationTime),
+                TimeUnit.MILLISECONDS.toSeconds((long) durationTime) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long)
+                                durationTime))));
+
+        // set progress to the seekbar
+        //seekBar.setProgress((int)startTime);
+        // set max value to the seekbar (max value is the duration of the sound)
+        seekBar.setMax((int) durationTime);
+
+        // update seekbar and textview while playing the sound
+        updateProgress();
+
+        // enable the pause button when playing sound
+        pauseButton.setEnabled(true);
+        // disable the play button - sound is already playing
+        playButton.setEnabled(false);
+    }
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            updateProgress();
+        }
+    };
+
+    // update progress in seekBar and time1TextView
+    private void updateProgress() {
+        startTime = mMediaPlayer.getCurrentPosition();
+        seekBar.setProgress((int)startTime);
+        // set the current time to the textview
+        time1TextView.setText(String.format("%d min %d s",
+                TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long)
+                                startTime))));
+        mHandler.postDelayed(runnable, 100);
     }
 }
