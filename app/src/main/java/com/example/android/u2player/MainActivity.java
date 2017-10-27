f55e417de2a036ced48797a3d6581e629475da36
marwysoc <marta.wysocka01@gmail.com>
2017-10-27 13:56:08 +0200

    Hide ActionBar

--- app/src/main/java/com/example/android/u2player/MainActivity.java
+++ app/src/main/java/com/example/android/u2player/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
